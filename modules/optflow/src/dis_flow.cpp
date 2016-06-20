9c23245be29fb73d17cea0b06342d8e4b76eb32e
Alexander Bokov <sbokov01@gmail.com>
2016-06-20 18:56:20 +0300

    Fixing the remaining buildbot warnings

--- modules/optflow/src/dis_flow.cpp
+++ modules/optflow/src/dis_flow.cpp
@@ -550,7 +550,7 @@ void DISOpticalFlowImpl::PatchGradientDescent_ParBody::operator()(const Range &r
 DISOpticalFlowImpl::Densification_ParBody::Densification_ParBody(DISOpticalFlowImpl &_dis, int _nstripes, int _h,
                                                                  Mat &dst_Ux, Mat &dst_Uy, Mat &src_Sx, Mat &src_Sy,
                                                                  Mat &_I0, Mat &_I1)
-    : dis(&_dis), nstripes(_nstripes), h(_h), Sx(&src_Sx), Sy(&src_Sy), Ux(&dst_Ux), Uy(&dst_Uy), I0(&_I0), I1(&_I1)
+    : dis(&_dis), nstripes(_nstripes), h(_h), Ux(&dst_Ux), Uy(&dst_Uy), Sx(&src_Sx), Sy(&src_Sy), I0(&_I0), I1(&_I1)
 {
     stripe_sz = (int)ceil(h / (double)nstripes);
 }
